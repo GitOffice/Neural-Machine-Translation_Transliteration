b01d22f7f3abd91767d1c23d5c6f90c28f9e3b4e
Stoufa <stoufa@users.noreply.github.com>
2018-05-04 01:09:26 +0100

    Added the high-level classes of the application
    * Added the files: {Translator, Tokenizer, Embedder}.py
        - Tokenizer deals with both tokenization (text -> {} of tokens) and untokenization ({} of tokens -> text)
        - Embedder deals with both embedding (token -> corresponding embedding vector) and unembedding (embedding vector -> corresponding token)

---
+++ Translator.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  4 00:39:26 2018
+
+@author: stoufa
+"""
+
+from Tokenizer import Tokenizer
+from Embedder import Embedder
+
+class Translator(object):
+	
+	def __init__(self):
+		pass
+
+	@staticmethod
+	def translate(inputText):
+		tokens = Tokenizer.tokenize(inputText)
+		#tokensEmbeddings = map(Embedder.embed, tokens)
+		#return list(tokensEmbeddings)
+		
+		# pass the embeddings through the neural network and get the results
+		# unembed the translated words' embedding vectors to words
+		# untokenize the translated tokens !
+		
+		return Tokenizer.untokenize(tokens)
+
+if __name__ == '__main__':
+	text = input('>>> ')
+	
+	# Translating
+	translatedText = Translator.translate(text)
+	print(f'translatedText: {translatedText}')
